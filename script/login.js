255b0592172f36f2a6d59f4e5b1b10213fe91e59
zakariyazubair <149674746+zakariyazubair@users.noreply.github.com>
2023-11-02 15:49:09 +0500

    Add files via upload

---
+++ script/login.js
@@ -0,0 +1,25 @@
+(function(){
+	 
+    $("#login-1").on("click", function() {
+      $(".full-screen").fadeToggle( "fast").css('display', 'block');
+  
+    });
+    
+  })();
+  (function(){
+   
+   $(".closeBtn-1").on("click", function() {
+     $(".full-screen").fadeToggle( "fast").css('display', 'none');
+ 
+   });
+   
+ })();
+ (function(){
+   
+   $(".closeBtn").on("click", function() {
+     $(".full-screen").fadeToggle( "fast").css('display', 'none');
+ 
+   });
+   
+ })();
+
---
+++ script/menu-drop-icon.js
@@ -0,0 +1,5 @@
+(function() {
+    $("#menu-drop").on("click", function() {
+       $(".nav-bar").slideUp("5000", "swing").css('display', 'flex');
+    });
+ })();
