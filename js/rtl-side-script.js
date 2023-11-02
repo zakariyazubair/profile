79e72f49c55cb25a2d93c886262c1501e14686a3
zakariyazubair <149674746+zakariyazubair@users.noreply.github.com>
2023-11-02 15:59:09 +0500

    Add files via upload

---
+++ js/rtl-side-script.js
@@ -0,0 +1,10 @@
+$('.autoplay-1').slick({
+  slidesToShow: 6,
+  slidesToScroll: 4,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots:true,
+  arrows:false,
+  dotsClass:'slick-dots-1',
+rtl:true,
+});
